509048421860a659739f38e02ad869cc9d403b9b
sofianesid94 <46898814+sofianesid94@users.noreply.github.com>
2019-02-06 22:25:49 +0100

    Update cv.js

--- cv.js
+++ cv.js
@@ -1,7 +1,20 @@
 window.addEventListener("scroll", function() { //ajoute une fonction efectuée lorsque l'utilisateur scroll sur la page
 
 // Calculez le déplacement ici ...
-let deplacement = ((window.scrollY)*100)/(document.body.offsetHeight-window.innerHeight);
+	let deplacement = (window.scrollY / (document.body.offsetHeight - window.innerHeight))*100;
+// Modifie la valeur de la barre de progression pour qu'elle corresponde au déplacement du scroll
 
-document.getElementById('progress').value = deplacement; // Modifie la valeur de la barre de progression pour qu'elle corresponde au déplacement du scroll
+	document.getElementById('progress').value = deplacement;
+	
+
+	console.log(deplacement);// Affiche le contenu de la variable maVariable dans la console du navigateur 
 });
+// Définition de la fonction de scroll
+function myScroll() { 
+	
+// Modifie la valeur de la barre de progression pour qu'elle corresponde au déplacement du scroll
+	var progress = document.getElementById('progress').value;
+// valeur de progress de la bare qui permet de changer et modifier la page avec scroll	
+	var value =(progress * (document.body.offsetHeight - window.innerHeight))/100;
+	window.scroll(0,value);
+}
